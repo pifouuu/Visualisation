819e7b0c84814bc6a0012f38fff4fac9a3de0139
pifouuu <pierre00fournier@gmail.com>
2017-03-03 10:26:41 +0100

    add bools

--- src/main.cc
+++ src/main.cc
@@ -16,7 +16,7 @@
 #include <boost/serialization/utility.hpp>
 #include "../include/gnuplot-iostream.h"
 
-#define NUMACTIONS 8
+#define NUMACTIONS 10
 
 int main() {
 	Gnuplot gp;
@@ -28,104 +28,131 @@ int main() {
 	actions.push_back(std::string("PUT_DOWN"));
 	actions.push_back(std::string("PUT_IN"));
 	actions.push_back(std::string("LOOK_RED_BLOCK_0"));
-	//actions.push_back(std::string("LOOK_RED_BLOCK_1"));
+	actions.push_back(std::string("LOOK_RED_BLOCK_1"));
 	actions.push_back(std::string("LOOK_BLUE_BLOCK_0"));
-	//actions.push_back(std::string("LOOK_BLUE_BLOCK_1"));
+	actions.push_back(std::string("LOOK_BLUE_BLOCK_1"));
 	//actions.push_back(std::string("LOOK_BLUE_BLOCK_2"));
 
-	std::vector<std::vector<std::pair<float,float>>> act_try(NUMACTIONS);
+	bool ACT_ACC = true;
+	bool ACT_TRY = true;
+	bool ACT_SUC = true;
+	bool BLOCKS_IN = false;
+	bool BLOCKS_RIGHT = false;
+	bool MODEL_ACC = false;
+
 	std::string dirname = "../myTexplore/";
-	std::string name = dirname+"no_tutor_v_0.000000_n_0.000000_act_try_";
-	for (int i=0; i<NUMACTIONS; i++){
-		std::ifstream ifs(name+actions[i]+".ser");
-		boost::archive::text_iarchive ia(ifs);
-		ia & act_try[i];
+	std::string name;
+	std::ifstream ifs;
+
+	if (ACT_TRY){
+		std::vector<std::vector<std::pair<float,float>>> act_try(NUMACTIONS);
+
+		std::string name = dirname+"no_tutor_v_0.000000_n_0.000000_act_try_";
+		for (int i=0; i<NUMACTIONS; i++){
+			ifs.open(name+actions[i]+".ser");
+			boost::archive::text_iarchive ia(ifs);
+			ia & act_try[i];
+			ifs.close();
+			ifs.clear();
+		}
+
+		gp << "set xrange [0:3000]\nset yrange [0:300]\n";
+		// Data will be sent via a temporary file.  These are erased when you call
+		// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
+		// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
+		// and won't be deleted (this is useful when creating a script).
+		gp << "plot";
+		for (int i=3;i<4;i++){
+			gp << gp.file1d(act_try[i]) << "with lines title 'act_try_" << actions[i] <<"',";
+		}
+		gp << std::endl;
 	}
 
-	gp << "set xrange [0:100]\nset yrange [0:30]\n";
-	// Data will be sent via a temporary file.  These are erased when you call
-	// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
-	// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
-	// and won't be deleted (this is useful when creating a script).
-	gp << "plot";
-	for (int i=3;i<NUMACTIONS;i++){
-		gp << gp.file1d(act_try[i]) << "with lines title 'act_try_" << actions[i] <<"',";
-	}
-	gp << std::endl;
+	if (ACT_ACC){
+		std::vector<std::vector<std::pair<float,float>>> act_acc(NUMACTIONS);
+		name = dirname+"no_tutor_v_0.000000_n_0.000000_act_acc_";
+		for (int i=3; i<6; i++){
+			ifs.open(name+actions[i]+".ser");
+			boost::archive::text_iarchive ia(ifs);
+			ia & act_acc[i];
+			ifs.close();
+			ifs.clear();
+		}
 
-
-	std::vector<std::vector<std::pair<float,float>>> act_acc(NUMACTIONS);
-	name = dirname+"no_tutor_v_0.000000_n_0.000000_act_acc_";
-	for (int i=0; i<NUMACTIONS; i++){
-		std::ifstream ifs(name+actions[i]+".ser");
-		boost::archive::text_iarchive ia(ifs);
-		ia & act_acc[i];
-	}
-
-	gp << "set xrange [0:3000]\nset yrange [0:1.5]\n";
-	gp << "set terminal x11 1\n";
-	// Data will be sent via a temporary file.  These are erased when you call
-	// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
-	// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
-	// and won't be deleted (this is useful when creating a script).
-	gp << "plot";
-	for (int i=0;i<NUMACTIONS;i++){
-		gp << gp.file1d(act_acc[i]) << "with lines title 'act_acc_" << actions[i]<<"',";
-	}
-	gp << std::endl;
-
-
-	std::vector<std::vector<std::pair<float,float>>> act_suc(NUMACTIONS);
-	name = dirname+"no_tutor_v_0.000000_n_0.000000_act_succes_";
-	for (int i=0; i<NUMACTIONS; i++){
-		std::ifstream ifs(name+actions[i]+".ser");
-		boost::archive::text_iarchive ia(ifs);
-		ia & act_suc[i];
+		gp << "set xrange [0:3000]\nset yrange [0:1.5]\n";
+		gp << "set terminal x11 1\n";
+		// Data will be sent via a temporary file.  These are erased when you call
+		// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
+		// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
+		// and won't be deleted (this is useful when creating a script).
+		gp << "plot";
+		for (int i=3;i<4;i++){
+			gp << gp.file1d(act_acc[i]) << "with lines title 'act_acc_" << actions[i]<<"',";
+		}
+		gp << std::endl;
 	}
 
-	gp << "set xrange [0:5000]\nset yrange [0:20]\n";
-	gp << "set terminal x11 2\n";
+	if (ACT_SUC){
+		std::vector<std::vector<std::pair<float,float>>> act_suc(NUMACTIONS);
+		name = dirname+"no_tutor_v_0.000000_n_0.000000_act_succes_";
+		for (int i=3; i<4; i++){
+			ifs.open(name+actions[i]+".ser");
+			boost::archive::text_iarchive ia(ifs);
+			ia & act_suc[i];
+			ifs.close();
+			ifs.clear();
+		}
 
-	// Data will be sent via a temporary file.  These are erased when you call
-	// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
-	// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
-	// and won't be deleted (this is useful when creating a script).
-	gp << "plot";
-	for (int i=0;i<NUMACTIONS;i++){
-		gp << gp.file1d(act_suc[i]) << "with lines title 'act_succes_" << actions[i]<<"',";
+		gp << "set xrange [0:3000]\nset yrange [0:300]\n";
+		gp << "set terminal x11 2\n";
+
+		// Data will be sent via a temporary file.  These are erased when you call
+		// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
+		// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
+		// and won't be deleted (this is useful when creating a script).
+		gp << "plot";
+		for (int i=0;i<NUMACTIONS;i++){
+			gp << gp.file1d(act_suc[i]) << "with lines title 'act_succes_" << actions[i]<<"',";
+		}
+		gp << std::endl;
 	}
-	gp << std::endl;
 
+	if (BLOCKS_IN){
+		std::vector<std::pair<float,float>> blocks_in;
+		ifs.open(dirname+"no_tutor_v_0.000000_n_0.000000_blocks_in.ser");
+		boost::archive::text_iarchive ia_in(ifs);
+		ia_in & blocks_in;
+		gp << "set xrange [0:100000]\nset yrange [0:1]\n";
+		gp << "set terminal x11 3\n";
+		gp << "plot" << gp.file1d(blocks_in) << "with lines title 'blocks_in'"<<std::endl;
+		ifs.close();
+		ifs.clear();
+	}
 
-	std::vector<std::pair<float,float>> blocks_in;
-	std::ifstream ifs(dirname+"no_tutor_v_0.000000_n_0.000000_blocks_in.ser");
-	boost::archive::text_iarchive ia_in(ifs);
-	ia_in & blocks_in;
-	gp << "set xrange [0:5000]\nset yrange [0:1]\n";
-	gp << "set terminal x11 3\n";
-	gp << "plot" << gp.file1d(blocks_in) << "with lines title 'blocks_in'"<<std::endl;
-	ifs.close();
-	ifs.clear();
+	if (BLOCKS_RIGHT){
+		std::vector<std::pair<float,float>> blocks_right;
+		ifs.open(dirname+"no_tutor_v_0.000000_n_0.000000_blocks_right.ser");
+		boost::archive::text_iarchive ia_right(ifs);
+		ia_right & blocks_right;
+		gp << "set xrange [0:100000]\nset yrange [0:5]\n";
+		gp << "set terminal x11 4\n";
+		gp << "plot" << gp.file1d(blocks_right) << "with lines title 'blocks_right'"<<std::endl;
+		ifs.close();
+		ifs.clear();
+	}
 
-	std::vector<std::pair<float,float>> blocks_right;
-	ifs.open(dirname+"no_tutor_v_0.000000_n_0.000000_blocks_right.ser");
-	boost::archive::text_iarchive ia_right(ifs);
-	ia_right & blocks_right;
-	gp << "set xrange [0:5000]\nset yrange [0:5]\n";
-	gp << "set terminal x11 4\n";
-	gp << "plot" << gp.file1d(blocks_right) << "with lines title 'blocks_right'"<<std::endl;
-	ifs.close();
-	ifs.clear();
+	if (MODEL_ACC) {
+		std::vector<std::pair<float,float>> model_acc;
+		ifs.open(dirname+"no_tutor_v_0.000000_n_0.000000_model_acc.ser");
+		boost::archive::text_iarchive ia_model(ifs);
+		ia_model & model_acc;
+		gp << "set xrange [0:100000]\nset yrange [0:7]\n";
+		gp << "set terminal x11 5\n";
+		gp << "plot" << gp.file1d(model_acc) << "with lines title 'model_error'"<<std::endl;
+		ifs.close();
+		ifs.clear();
+	}
 
-	std::vector<std::pair<float,float>> model_acc;
-	ifs.open(dirname+"no_tutor_v_0.000000_n_0.000000_model_acc.ser");
-	boost::archive::text_iarchive ia_model(ifs);
-	ia_model & model_acc;
-	gp << "set xrange [0:5000]\nset yrange [0:7]\n";
-	gp << "set terminal x11 5\n";
-	gp << "plot" << gp.file1d(model_acc) << "with lines title 'model_error'"<<std::endl;
-	ifs.close();
-	ifs.clear();
 
 }
 
